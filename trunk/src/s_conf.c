a6c0b71995c22bb03f1f86bced4af74a426c3c0a
therock247uk@gmail.com <therock247uk@gmail.com@155156e8-c7df-11de-afa3-455a29e9879b>
2009-11-02 21:05:46 +0000

    Removed silly crap now you can have whatever modes you like on oper even +S haha

--- trunk/src/s_conf.c
+++ trunk/src/s_conf.c
@@ -3407,7 +3407,7 @@ int	_test_oper(ConfigFile *conf, ConfigEntry *ce)
 			{
 				char *p;
 				for (p = cep->ce_vardata; *p; p++)
-					if (strchr("oOaANCrzS", *p))
+					if (strchr("", *p))
 					{
 						config_error("%s:%i: oper::modes may not include mode '%c'",
 							cep->ce_fileptr->cf_filename, cep->ce_varlinenum, *p);
@@ -7238,7 +7238,7 @@ int	_test_set(ConfigFile *conf, ConfigEntry *ce)
 			CheckNull(cep);
 			CheckDuplicate(cep, modes_on_connect, "modes-on-connect");
 			for (p = cep->ce_vardata; *p; p++)
-				if (strchr("oOaANCrzSgHhqtW", *p))
+				if (strchr("", *p))
 				{
 					config_error("%s:%i: set::modes-on-connect may not include mode '%c'",
 						cep->ce_fileptr->cf_filename, cep->ce_varlinenum, *p);
@@ -7304,7 +7304,7 @@ int	_test_set(ConfigFile *conf, ConfigEntry *ce)
 			CheckNull(cep);
 			CheckDuplicate(cep, modes_on_oper, "modes-on-oper");
 			for (p = cep->ce_vardata; *p; p++)
-				if (strchr("oOaANCrzS", *p))
+				if (strchr("", *p))
 				{
 					config_error("%s:%i: set::modes-on-oper may not include mode '%c'",
 						cep->ce_fileptr->cf_filename, cep->ce_varlinenum, *p);
